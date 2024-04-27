f57e42f9aab4b6922eb8d94e4306787f078edec4
KBTrotter <ktrotter@nevada.unr.edu>
2024-04-27 14:35:42 -0700

    included RTC Module code to display time and date

---
+++ RTC_Module.ino
@@ -0,0 +1,48 @@
+#include "RTClib.h"
+
+RTC_DS1307 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+void setupRTC() {
+  Serial.begin(9600);
+
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+
+  if (!rtc.isrunning()) {
+    Serial.println("RTC is NOT running, let's set the time!");
+    rtc.adjust(DateTime(2024, 4, 27, 2, 19, 0)); // Adjust as needed
+  }
+}
+
+void printDateTime() {
+  DateTime now = rtc.now();
+
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+}
+
+void setup() {
+  setupRTC();
+}
+
+void loop() {
+  printDateTime();
+  delay(3000);
+}
